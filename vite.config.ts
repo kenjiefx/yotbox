eacb78068be2fd4c477d4c9b48c0d9c5d187b44a
kenjiefx <terradoflex@gmail.com>
2026-05-29 14:08:31 +0800

    add: prepare support for external scripting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "./",
   plugins: [react()],
   build: {
+    minify: false,
     rollupOptions: {
       input: {
         popup: "popup.html",
+        // contextLoaderIndex: resolve(__dirname, "src/context-loader/index.ts"),
+        // contextLoaderInjector: resolve(
+        //   __dirname,
+        //   "src/context-loader/injector.ts",
+        // ),
       },
+      // output: {
+      //   entryFileNames: (chunkInfo) => {
+      //     if (chunkInfo.name === "contextLoaderIndex") {
+      //       return "context-loader/index.js";
+      //     }
+      //     if (chunkInfo.name === "contextLoaderInjector") {
+      //       return "context-loader/injector.js";
+      //     }
+      //     return "assets/[name]-[hash].js";
+      //   },
+      // },
     },
   },
 });
