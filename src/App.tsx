b34e2d76c4cd015b7db1258ef44a3acf7a09f331
kenjiefx <terradoflex@gmail.com>
2026-06-21 07:30:29 +0800

    added view logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Header from "./ui/layout/Header";
 import WidgetLibrary from "./features/widget-library/components/WidgetLibrary";
+import { useState } from "react";
+import { AppView } from "./types";
+import ReviewsWidgetEditor from "./features/widget-editors/reviews-main-widget/components/ReviewsWidgetEditor";
 
 export default function App() {
+  const [view, setView] = useState<AppView>("widget_library");
   return (
     <div id="extension-container" className="bg-gray-200">
-      <Header />
-      <WidgetLibrary />
+      {view === "widget_library" && (
+        <>
+          <Header />
+          <WidgetLibrary viewEditor={(editorView) => setView(editorView)} />
+        </>
+      )}
+      {view === "editor:reviews_main_widget" && (
+        <ReviewsWidgetEditor exitEditor={() => setView("widget_library")} />
+      )}
     </div>
   );
 }
